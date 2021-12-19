2949389cf2de046da84cfe3d866c6695e676ae06
Irina Saveleva <fomina_88@mail.ru>
2021-12-19 22:53:48 +0400

    added README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ppalmcph3omeq879?svg=true)](https://ci.appveyor.com/project/Saveleva88/aqa1-2-2)
--- src/test/java/ru/netology/PostApiTest.java
+++ src/test/java/ru/netology/PostApiTest.java
@@ -20,7 +20,7 @@ public class PostApiTest {
 
                 .then()
                 .statusCode(200)
-                .body("headers", equalTo("some data"))
+                .body("data", equalTo("some data"))
         ;
 
     }
